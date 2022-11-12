7a5e1792c391dd02ccd3cee81b76222781afb044
Polorion <97245263+Polorion@users.noreply.github.com>
2022-11-12 20:22:41 +0300

    addSound

--- src/component/ButtonControlerGame/ButtonControlGame.jsx
+++ src/component/ButtonControlerGame/ButtonControlGame.jsx
@@ -1,9 +1,22 @@
 import * as React from "react";
 import S from "./ButtonControlGame.module.scss";
+import { useRef } from "react";
+import jumpSound from "../../audio/drums.mp3";
 
 const ButtonControlGame = (props) => {
+  const audioRef = useRef();
+
+  const playSound = () => {
+    audioRef.current.play();
+    audioRef.current.volume = 0.05;
+  };
+  const stopSound = () => {
+    audioRef.current.pause();
+  };
+
   return (
     <div>
+      <audio loop="loop" ref={audioRef} src={jumpSound}></audio>
       <button
         style={{ position: "relative", zIndex: "100000" }}
         onClick={props.changeOwner}
@@ -18,7 +31,10 @@ const ButtonControlGame = (props) => {
       </button>{" "}
       <button
         style={{ position: "relative", zIndex: "100000" }}
-        onClick={props.changeRunGame}
+        onClick={() => {
+          props.changeRunGame();
+          props.gameIsRun ? stopSound() : playSound();
+        }}
       >
         {props.gameIsRun ? "пауза" : "начать игру"}
       </button>
--- src/component/MoveButton/MoveButton.jsx
+++ src/component/MoveButton/MoveButton.jsx
@@ -1,14 +1,26 @@
 import * as React from "react";
 import S from "./MoveButton.module.scss";
+import jumpSound from "../../audio/jump.mp3";
+import { useRef } from "react";
 
 const MoveButton = (props) => {
+  const audioRef = useRef();
+
+  const playSound = () => {
+    audioRef.current.playbackRate = 3.0;
+    audioRef.current.volume = 0.1;
+    audioRef.current.play();
+  };
   return (
     <div
       onClick={() => {
         props.action(props.position);
+        playSound();
       }}
       className={`${S.btn} ${props.position}`}
-    ></div>
+    >
+      <audio ref={audioRef} src={jumpSound}></audio>
+    </div>
   );
 };
 
